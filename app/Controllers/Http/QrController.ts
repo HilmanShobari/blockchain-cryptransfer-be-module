2210396b71a0d9689ea8c69fcad2f2cb4d7a1211
Hilman <hilman@loyalto.id>
2023-12-01 18:21:29 +0700

    add feature socket on module and add unique code handler

--- app/Controllers/Http/QrController.ts
+++ app/Controllers/Http/QrController.ts
@@ -1,3 +1,5 @@
+import Env from '@ioc:Adonis/Core/Env'
+
 export default class QrController {
   public static async createQr(request) {
     try {
@@ -8,11 +10,32 @@ export default class QrController {
         })
       }
 
+      let newValue: string
+      let addValue: string
+      let DigitsAfterDecimalPoint = 0
+
+      const randomNumber = this.generateRandomNumber(Env.get('UNIQUE_CODE_LENGTH'))
+
+      // check if the value has decimal
+      if (request.value.includes('.')) {
+        DigitsAfterDecimalPoint = request.value.split('.')[1].length
+        addValue =
+          this.generateZeroString(
+            parseInt(request.decimal) - randomNumber.toString().length - DigitsAfterDecimalPoint
+          ) + randomNumber
+        newValue = `${request.value + addValue}e${request.decimal}`
+      } else {
+        addValue =
+          this.generateZeroString(parseInt(request.decimal) - randomNumber.toString().length) +
+          randomNumber
+        newValue = `${request.value}.${addValue}e${request.decimal}`
+      }
+
       let qrValue: string
       if (!!request.tokenAddress) {
-        qrValue = `ethereum:${request.tokenAddress}@${request.chainId}/transfer?address=${request.destinationAddress}&uint256=${request.value}`
+        qrValue = `ethereum:${request.tokenAddress}@${request.chainId}/transfer?address=${request.destinationAddress}&uint256=${newValue}`
       } else {
-        qrValue = `ethereum:${request.destinationAddress}?value=${request.value}`
+        qrValue = `ethereum:${request.destinationAddress}?value=${newValue}`
       }
 
       return {
@@ -34,5 +57,27 @@ export default class QrController {
       status_code: error.status_code || 500,
       message: error.message || 'Internal Server Error',
     }
+  }
+
+  // random number generator
+  private static generateRandomNumber(length) {
+    const min = 10 ** (length - 1)
+    const max = 10 ** length - 1
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  // zero string generator
+  private static generateZeroString(digitCount) {
+    if (digitCount <= 0) {
+      return ''
+    }
+
+    let zeroString = ''
+    for (let i = 0; i < digitCount; i++) {
+      zeroString += '0'
+    }
+
+    return zeroString
   }
 }
