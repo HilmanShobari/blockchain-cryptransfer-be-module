59ccb8a206c47c74a5ae0bce30a58750a19f2331
Hilman <hilman@loyalto.id>
2023-12-06 15:04:02 +0700

    add truswallet handler qr

--- app/Controllers/Http/QrController.ts
+++ app/Controllers/Http/QrController.ts
@@ -23,19 +23,61 @@ export default class QrController {
           this.generateZeroString(
             parseInt(request.decimal) - randomNumber.toString().length - DigitsAfterDecimalPoint
           ) + randomNumber
-        newValue = `${request.value + addValue}e${request.decimal}`
+        newValue = `${request.value + addValue}`
       } else {
         addValue =
           this.generateZeroString(parseInt(request.decimal) - randomNumber.toString().length) +
           randomNumber
-        newValue = `${request.value}.${addValue}e${request.decimal}`
+        newValue = `${request.value}.${addValue}`
       }
 
-      let qrValue: string
-      if (!!request.tokenAddress) {
-        qrValue = `ethereum:${request.tokenAddress}@${request.chainId}/transfer?address=${request.destinationAddress}&uint256=${newValue}`
-      } else {
-        qrValue = `ethereum:${request.destinationAddress}?value=${newValue}`
+      let qrValue: string = ''
+      // using metamask
+      if (request.walletType === 'metamask') {
+        if (request.tokenAddress) {
+          qrValue = `ethereum:${request.tokenAddress}@${request.chainId}/transfer?address=${request.destinationAddress}&uint256=${newValue}e${request.decimal}`
+        } else {
+          qrValue = `ethereum:${request.destinationAddress}@${request.chainId}?value=${newValue}e${request.decimal}`
+        }
+        // using trustwallet
+      } else if (request.walletType === 'trustwallet') {
+        if (request.network === 'ethereum') {
+          // not native coin
+          if (request.tokenAddress) {
+            qrValue = `https://link.trustwallet.com/send?asset=${Env.get('ETHEREUM_UAI')}_t${
+              request.tokenAddress
+            }&address=${request.destinationAddress}&amount=${newValue}`
+            // native coin
+          } else {
+            qrValue = `https://link.trustwallet.com/send?asset=${Env.get('ETHEREUM_UAI')}&address=${
+              request.destinationAddress
+            }&amount=${newValue}`
+          }
+        } else if (request.network === 'binance') {
+          // not native coin
+          if (request.tokenAddress) {
+            qrValue = `https://link.trustwallet.com/send?asset=${Env.get('BINANCE_UAI')}_t${
+              request.tokenAddress
+            }&address=${request.destinationAddress}&amount=${newValue}`
+            // native coin
+          } else {
+            qrValue = `https://link.trustwallet.com/send?asset=${Env.get('BINANCE_UAI')}&address=${
+              request.destinationAddress
+            }&amount=${newValue}`
+          }
+        } else if (request.network === 'polygon') {
+          // not native coin
+          if (request.tokenAddress) {
+            qrValue = `https://link.trustwallet.com/send?asset=${Env.get('POLYGON_UAI')}_t${
+              request.tokenAddress
+            }&address=${request.destinationAddress}&amount=${newValue}`
+            // native coin
+          } else {
+            qrValue = `https://link.trustwallet.com/send?asset=${Env.get('POLYGON_UAI')}&address=${
+              request.destinationAddress
+            }&amount=${newValue}`
+          }
+        }
       }
 
       return {
